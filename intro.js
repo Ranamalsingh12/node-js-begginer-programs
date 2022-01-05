5afa3ce0fdfd8729791fe716005ac464fd69282d
ranamalsingh12 <ranams99911@gmail.com>
2022-01-05 12:04:58 +0530

    commit

--- intro.js
+++ intro.js
@@ -5,8 +5,9 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.end('Hello World');
 });
 
