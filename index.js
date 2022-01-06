07d839ac9d30a0cfa1262bf73adf63a5df20d0ca
ranamalsingh12 <ranams99911@gmail.com>
2022-01-06 12:44:28 +0530

    creating modules

---
+++ index.js
@@ -0,0 +1,5 @@
+const currency = require('./currency');
+console.log('50 canadian dollar equal to this amount of us dollars');
+console.log(currency.canadianToUS(50));
+console.log('20 us dollar equal to this amount of canadian dollars');
+console.log(currency.UstoCanadian(20));
